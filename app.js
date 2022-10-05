64a1fa344da6a9cd6620e654d6dfc8a5b8cdcd8b
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-05 21:30:15 +0200

    done

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const btn = document.querySelector(".form-shorten button");
 
 const newShorten  =document.querySelector(".newShorten");
 
+const myDiv = document.querySelectorAll(".myDiv")
+
 let myAdress; 
+let button;
+
 
 
 const myShorten = async ()=>{
 
 myAdress = shorten.value;
 
-const url = `https://api.shrtco.de/v2/shorten?url=${myAdress}/very/long/link.html`;
+const url = `https://api.shrtco.de/v2/shorten?url=${myAdress}`;
 
   
     try {
@@ -27,33 +31,48 @@ const url = `https://api.shrtco.de/v2/shorten?url=${myAdress}/very/long/link.htm
         result:{full_short_link}} = data
         console.log(short_link);
 
+        // newShorten.innerText == "" ? newShorten.classList.add("form-shorten") :newShorten.classList.remove("form-shorten")
+
+        newShorten.classList.add("form-shorten");
+
+        const myDiv = document.createElement("div");
+        newShorten.appendChild(myDiv);
+        myDiv.classList.add("myDiv")
+
+        const trash = document.querySelector("p");
+        trash.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+        myDiv.prepend(trash)
+
         const copy = document.createElement("button");
         copy.classList.add("copy");
         copy.innerText ="Copy"
-        newShorten.prepend(copy);
+        myDiv.prepend(copy);
 
         const link = document.createElement("a");
         link.href = full_short_link;
         link.innerText = short_link;
         link.target = "_blank";
-        newShorten.prepend(link)
+        myDiv.prepend(link)
 
         const par = document.createElement("p");
+        par.classList.add("par");
         par.innerText = shorten.value;
-        newShorten.prepend(par);
+        myDiv.prepend(par);
 
         shorten.value =""
 
-       
-
+        copy.addEventListener("click",()=>{
+            short_link.select();
+            short_link.setSelectionRange(0,9999);
+            navigator.clipboard.writeText(short_link);
+            alert("Copied")
+        })
  
     } 
      
     catch (error) {
         console.log(error);
     }
-
-
 
 }
 
@@ -62,4 +81,27 @@ btn.addEventListener("click",(e) =>{
 
     myShorten()
 });
+
+newShorten.addEventListener("click" , (e)=>{
+    if(e.target.classList.contains("fa-trash-can")){
+        e.target.parentElement.parentElement.remove()
+    }
+
+})
+
+
+// function myFunction() {
+//     // Get the text field
+    
+  
+//     // Select the text field
+//     copyText.select(); 
+//     copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+//      // Copy the text inside the text field
+//     navigator.clipboard.writeText(copyText.value);
+  
+//     // Alert the copied text
+//     alert("Copied the text: " + copyText.value);
+//   }
 
